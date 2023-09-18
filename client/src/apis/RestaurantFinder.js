525d388ab3016cb9a7a9323681e0edc5001a95b1
andrewwon79 <andrewwon79@gmail.com>
2023-09-17 19:00:42 -0700

    latest did not get uploaded, fixing

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 //axios makes apis return look nice, must npm install axios
+
+//when deploying, our production won't know what localhost api is
+//We're going to have a variable telling us if we're in prod or not
+// NODE_ENV = 'development'
+// NODE_EVN = 'production'
+
+// if we are in production baseurl = "/api/v1/restaurants"
+// else baseurl = "http://localhost:3006/api/v1/restaurants"
+
+
+const baseURL = process.env.NODE_ENV === 'production' ? "api/v1/restaurants" : "http://localhost:3006/api/v1/restaurants";
+//const baseURL = "/api/v1/restaurants";
 export default axios.create({
-    baseURL: "http://localhost:3006/api/v1/restaurants",
+    //equivalent to baseURL: baseURL
+    baseURL,
 });
